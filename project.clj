bbc9993e35021a6c0c3d0ad11914cebc7b7f0903
Muchenxuan Tong <demon386@gmail.com>
2012-09-22 20:33:42 +0800

    init-dirs when startup.

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [enlive "1.0.1"]
                  [org.clojure/data.xml "0.0.6"]
-                 [clj-time "0.4.4"]]
+                 [clj-time "0.4.4"]
+                 [fs "1.3.2"]]
   :main neverland.main)
--- src/neverland/main.clj
+++ src/neverland/main.clj
@@ -5,14 +5,34 @@
   (:require [neverland.extracter :as extracter]
             [neverland.post :as post]
             [neverland.pager :as pager]
-            [neverland.render :as render])
+            [neverland.render :as render]
+            [fs.core :as fs])
   (:gen-class))
+
+(def reserve-file-patterns ["bootstrap" "org.css" "mycss.css"])
+
+(defn init-dirs []
+  "Clean the html folder, delete everything except reserved. Create new dirs."
+  (let [to-del (filter (comp not #(some #{%} reserve-file-patterns))
+                       (fs/list-dir "html"))]
+    (dorun (map fs/delete-dir
+                (map #(str "html/" %) to-del)))
+    (fs/mkdir "html/posts")))
 
 (defn all-orghtml-files []
   (neverland.io/walk "orghtml" #"[^.].*\.html"))
 
+(defn publish-resources []
+  "Publish all the folders in orghtml."
+  (let [res-dirs (filter fs/directory?
+                         (map #(str "orghtml/" %)
+                              (fs/list-dir "orghtml")))]
+    (dorun (map #(fs/copy-dir % "html") res-dirs))))
+
 (defn -main [& args]
   (when-let [postrecords (map post/to-postrecord-from-file
                               (all-orghtml-files))]
+    (init-dirs)
     (render/render postrecords)
-    (rss postrecords)))
+    (rss postrecords)
+    (publish-resources)))
